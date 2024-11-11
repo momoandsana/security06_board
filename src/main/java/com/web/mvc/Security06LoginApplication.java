2c7b03399fa0c848649892a03fe653a960b8dae4
hoseok <swiftie52@naver.com>
2024-11-11 12:12:49 +0900

    initial commit

---
+++ src/main/java/com/web/mvc/Security06LoginApplication.java
@@ -0,0 +1,13 @@
+package com.web.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Security06LoginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Security06LoginApplication.class, args);
+    }
+
+}
